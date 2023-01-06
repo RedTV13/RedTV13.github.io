5f87233a8eca8f419f126ef95cfe77e90eb2f0b4
RedTV13 <121776050+RedTV13@users.noreply.github.com>
2023-01-06 12:30:28 +1100

    Update script.js

--- Organiser/script.js
+++ Organiser/script.js
@@ -11,6 +11,7 @@ var intHoursCount = 0;
 var arrHours = [0,1,2,3,4,5]
 var arrMinutes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]
 var arrSeconds = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]
+var elem = document.documentElement;
 
 function StartUp()
 {
@@ -170,4 +171,38 @@ function reset()
   intSecondsCount = 0;
   intMinutesCount = 0;
   intHoursCount = 0;
+}
+
+/* View in fullscreen */
+function openFullscreen() 
+{
+  if (elem.requestFullscreen) 
+  {
+    elem.requestFullscreen();
+  } 
+  else if (elem.webkitRequestFullscreen) 
+  { /* Safari */
+    elem.webkitRequestFullscreen();
+  } 
+  else if (elem.msRequestFullscreen) 
+  { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() 
+{
+  if (document.exitFullscreen) 
+  {
+    document.exitFullscreen();
+  } 
+  else if (document.webkitExitFullscreen) 
+  { /* Safari */
+    document.webkitExitFullscreen();
+  } 
+  else if (document.msExitFullscreen) 
+  { /* IE11 */
+    document.msExitFullscreen();
+  }
 }
