471290952e2f4ceea071f1779d6029eac99df332
u7622754 <u7622754@anu.edu.au>
2024-01-09 21:25:19 +1100

    updated the whole thing

--- School/script.js
+++ School/script.js
@@ -1,4 +1,22 @@
 function StartUp()
 {
-    document.getElementById("Bar").focus();
+    document.getElementsByTagName("input")[0].focus();
 }
+
+function showMenu()
+{
+    document.getElementById("menu").classList.toggle("hidden");
+}
+
+function search(e)
+{
+    window.open("https://www.bing.com/search?q=" + e, "_self")
+}
+
+document.getElementById("search").addEventListener("keydown", (e) =>
+{
+    if (e.key == "Enter")
+    {
+        search(document.getElementById("search").value);
+    }
+})
